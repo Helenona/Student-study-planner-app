ac112f3232be6010db474483c241a755629f64dd
Helen Onanusi <H.onanusi@edu.salford.ac.uk>
2026-04-26 15:50:01 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function goTo(screenId) {
+    let screens = document.querySelectorAll(".screen");
+    screens.forEach(screen => screen.classList.remove("active"));
 
+    document.getElementById(screenId).classList.add("active");
+}
+
+function addAssignment() {
+    let input = document.getElementById("newAssignment");
+    let list = document.getElementById("assignmentList");
+
+    if (input.value !== "") {
+        let li = document.createElement("li");
+        li.textContent = input.value;
+        list.appendChild(li);
+        input.value = "";
+    }
+}
+
+function addSession() {
+    let input = document.getElementById("newSession");
+    let list = document.getElementById("sessionList");
+
+    if (input.value !== "") {
+        let li = document.createElement("li");
+        li.textContent = input.value;
+        list.appendChild(li);
+        input.value = "";
+    }
+}
